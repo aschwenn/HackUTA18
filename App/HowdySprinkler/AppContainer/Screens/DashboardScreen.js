9d6d03d4f51028ba4323286a9f4bde80c6d47ed4
Alex Le <kawoofish@gmail.com>
2018-10-07 11:37:20 -0500

    Fake data example

--- App/HowdySprinkler/AppContainer/Screens/DashboardScreen.js
+++ App/HowdySprinkler/AppContainer/Screens/DashboardScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, FlatList } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import { createStackNavigator } from 'react-navigation';
 import { Container, Button, Content, Text, Icon, List, ListItem } from 'native-base';
@@ -58,33 +58,73 @@ export default class DashboardScreen extends Component {
       .then((responseJson) => {
          console.log(responseJson);
 
-         plants = responseJson;
+         plantsArr = responseJson;
 
          this.setState({
             plants: plantsArr,
          })
+
+        /*
+         let j = plants.length;
+         for(i = 0; i < j;i++){
+           plantsArr = [];
+           this.setState({
+              amount: responseJson[i].amount,
+              amount2: responseJson[i].amount2,
+              id: responseJson[i].id,
+              moisture: responseJson[i].moisture,
+              name: responseJson[i].name,
+              period: responseJson[i].period,
+              scheduled: responseJson[i].scheduled,
+              tank_low: responseJson[i].tank_low,
+              water_immediate: responseJson[i].water_immediate,
+           })
+          plantsArr = plants.map()
+         }
+
+         plants.foreach((p) => {
+           this.setState({
+              amount: p.amount,
+              amount2: p.amount2,
+              id: p.id,
+              moisture: p.moisture,
+              name: p.name,
+              period: p.period,
+              scheduled: p.scheduled,
+              tank_low: p.tank_low,
+              water_immediate: p.water_immediate,
+           })
+         });*/
       })
       .catch((error) => {
          console.error(error);
       });
   }
 
-  openPlantInfoScreen = (doc) => {
+  /*openPlantInfoScreen = (doc) => {
     this.props.navigation.push({
       screen: 'PlantInfo',
       backButtonTitle: '',
       passProps: {doc: doc},
       title: doc.title
     })
-  }
+  }*/
 
   render() {
+    var constants = [
+      'Albert',
+      'Alfred',
+      'Alex',
+      'Allie',
+      'Andrew',
+      'Mandy',
+    ]
     return (
       <Container>
-      <StatusBar backgroundColor='#11998e' barStyle='light-content' />
-          <LinearGradient colors={['#11998e', '#38ef7d']} style={styles.container}>
-          <Text style={styles.dashboard}>Plant Dashboard</Text>
-          </LinearGradient>
+          <List dataArray={constants} renderRow={(item) =>
+              <ListItem button onPress={()=>this.setModalVisible(true, item)} >
+              <Text>{item}</Text>
+                </ListItem> } />
       </Container>
     );
   }
